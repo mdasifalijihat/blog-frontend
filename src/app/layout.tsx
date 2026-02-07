435ff6fa82715a8f731362530b8ab6892ecbd27f
mdasifalijihat <mdasifalijihat@gmail.com>
2026-02-07 18:22:14 +0600

    added appsitebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Navbar } from "@/components/layout/navbar";
+
 import { ThemeProvider } from "@/providers/theme-provider";
 
 const geistSans = Geist({
@@ -35,7 +35,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <Navbar />
+        
           {children}
         </ThemeProvider>
       </body>
