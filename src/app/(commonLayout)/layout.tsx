1c2852eba5c819570f2d754ab3971e2da6518d34
mdasifalijihat <mdasifalijihat@gmail.com>
2026-02-09 14:29:12 +0600

    added footer

--- src/app/(commonLayout)/layout.tsx
+++ src/app/(commonLayout)/layout.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "@/components/layout/footer";
 import { Navbar } from "@/components/layout/navbar";
 import React from "react";
 
@@ -10,6 +11,7 @@ export default function CommonLayout({
     <div>
       <Navbar />
       {children}
+      <Footer />
     </div>
   );
 }
